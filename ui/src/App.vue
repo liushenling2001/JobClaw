c3d794157715a7798a87bdc96e8d235a26acf348
shenlingliu <22607104@qq.com>
2026-03-31 22:44:23 +0800

    chore: remove ui source folder from repository

--- ui/src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <router-view />
-</template>
-
-<script setup lang="ts">
-// 根组件，仅渲染路由视图
-</script>
--- ui/src/components/common/Card.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="glass-panel p-4 rounded-lg border border-outline-variant/20 shadow-[inset_0_1px_4px_rgba(255,255,255,0.05)]">
-    <slot />
-  </div>
-</template>
-
-<script setup lang="ts">
-// 卡片容器组件
-</script>
